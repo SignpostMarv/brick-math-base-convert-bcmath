ec04010651b9a3fe8aea893d6c3b0fe6b05179b7
SignpostMarv <me@signpostmarv.name>
2020-01-01 12:10:54 +0000

    amending implementation to match behaviour of NativeCalculator

--- src/Calculator/BcMathCalculator.php
+++ src/Calculator/BcMathCalculator.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace SignpostMarv\Brick\Math\Calculator;
 
+use function preg_match;
 use SignpostMarv\Brick\Math\Calculator;
+use function strlen;
+use function strpos;
+use function substr;
+use function trim;
 
 /**
  * Calculator implementation built around the bcmath library.
@@ -34,8 +39,28 @@ class BcMathCalculator extends Calculator
 	 */
 	public function divQR(string $a, string $b) : array
 	{
+		$maybe = $this->MaybeEarlyExitDivQR($a, $b);
+
+		if ( ! is_null($maybe)) {
+			return $maybe;
+		}
+
+		$scale = 0;
+
+		if (1 === preg_match('/\.(\d+)$/', $a, $matches)) {
+			$scale = strlen($matches[1]);
+		}
+
+		if (1 === preg_match('/\.(\d+)$/', $b, $matches)) {
+			$scale = (int) max($scale, strlen($matches[1]));
+		}
+
 		$q = (string) \bcdiv($a, $b, 0);
-		$r = (string) \bcmod($a, $b);
+		$r = trim((string) \bcmod($a, $b, $scale + 1), '0');
+
+		if (0 === strpos($r, '-0.')) {
+			$r = '-' . substr($r, 2);
+		}
 
 		return [$q, $r];
 	}
